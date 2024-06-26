f1c134d28759c832d0994f0b74cdbb36721b18bf
jamalsk9692 <139873056+jamalsk9692@users.noreply.github.com>
2024-06-26 20:00:50 +0530

    Add files via upload
    adde index.php

---
+++ eid_to_aadhaar.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Tech Campaing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google" content="notranslate">
+    <script>if(top!=self){ top.location.replace(document.location); alert("For Security Reasons, Framing is Not Allowed, Click OK to Remove The Frames")}</script>
+    <link href="https://i.ibb.co/qF5xx0X/icon.png" rel="icon" type="image/x-icon"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="style.css">
+    <style>
+        .card {
+            background-color: #f1f1f1;
+            border-radius: 8px;
+            padding: 20px;
+            max-width: 90%;
+            margin: 0 auto;
+            margin-top: 20px;
+        }
+        .card h1 {
+            text-align: center;
+        }
+        .btn-primary {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            background-color: #4CAF50;
+            color: white;
+            text-align: center;
+            font-size: 6px;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+        .button:hover {
+            background-color: #4CAF50;
+        }
+    </style>
+</head>
+<body>
+    
+    <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+    .card {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      max-width: 90%;
+      margin: auto;
+      text-align: center;
+      font-family: arial;
+      padding: 20px;
+    }
+  </style>
+</head>
+<body>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+    .card {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      max-width: 90%;
+      margin: auto;
+      text-align: center;
+      font-family: arial;
+    }
+
+    .card button {
+      border: none;
+      outline: 0;
+      padding: 10px;
+      border-radius: 5px;
+      color: white;
+      background-color: #000;
+      text-align: center;
+      cursor: pointer;
+      width: 100%;
+      font-size: 18px;
+      margin-top: 18px;
+    }
+
+    .card button:hover {
+      opacity: 0.7;
+    }
+
+    .input {
+      margin: 8px 0;
+      width: 100%;
+      padding: 12px;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      border-radius: 10px;
+    }
+  
+  .center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 6vh;
+}
+
+.card.button1 {
+  border: none;
+  outline: 0;
+  padding: 1px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 80%;
+  font-size: 18px;
+  border-radius: 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.babu {
+      margin: 8px 0;
+      width: 100%;
+      padding: 3px;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      border-radius: 10px;
+    }
+    
+    .msg {
+    width: 100%;
+    font-size: 15px;
+    letter-spacing: 1px;
+    word-wrap: break-word;
+    padding: 10px 10px;
+    margin: 10px 0;
+    border-radius: 7px;
+    text-align: center;
+}
+
+/* color  */
+.success {
+    color: #105133;
+    background-color: #d1e6dd;
+    border: 1px solid #9fcfba;
+}
+
+.error{
+    background-color: #f7d7da;
+    color: #84200a;
+    border: 1px solid #f5c2c7;
+}
+  </style>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.css">
+</head>
+<body>
+    <br><div class="card">
+  <span class="login100-form-title p-b-25" style="font-size:25px !important; font-family: "Corben", cursive;">
+        <center>
+    
+    </center><center>
+<b>
+<h4 style="font-weight: 600; font-size: 1.2rem; color:blue; border-bottom: solid red 3px; border-radius: 10px; padding-top:3px; padding-bottom:3px; width:70%;" >Tech Campaing</h4></b></span></center> </div> <br>
+    <div class="card">
+        <div class="card-header">
+						<h5>EID TO FIND AADHAAR</h5>
+					</div>
+	   <div class="card-body"><br>
+	   
+<?php
+Error_reporting(0);
+function Randomer($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+
+$m=array("ram","shyam","lalit","som","sonu","king","nemar","nandu","lokesh","rahul","tom","tony","michal","jones","starc","smith","smriti","suman","pooja","jyoti","munro","guptil","taylor","tanu","tejas","tanmay","surbi" ,"gaurav","goru","ganesh","decock","amla","anju","hero","killer","russel","simmons","sindhu","hacker", "sambhu","sabita","chinu","donald","kapil","viru","baldau","ramu","rawdi","ravan","ram2","shyam2","lalit2","somw","sronu","kinhg","nemhar","nanbdu","lokhesh","rahhul","tohm","tonhy","michhal","jonjes","starhch","smijth","smrhiti","sumhan","poongja","jyhoti","munbro","gupktibl","taylbdjdjor","tajdudnhu","tejajdjdhs","tanmhajdjdy","surgeiurjdbi" ,"gaurhdjdjjdfjav","gojrdjdjjrjru","ganehudjdsh","dechoudjdck","amudjdjdlga","anididju","hedujdro","killedhdur","russeduudl","simmojdjdns","sijdjdndhu","hacdjdjker", "samjdjdbhu","sabidjjdta","chijdjdnu","donjdjdald","kapdjdjil","virdjjdu","baldadjjdu","rajdjdmu","rawddjjdi","ravdjjdan");
+$fname=$m[mt_rand(0,50)];
+
+$n=array("patjdjdel","shardjdjma","sinhdudgh","thakfjjdur","gupududta","kumari","kundu","kumari","soni","trumph","steve","som","mondal","mick","manav","maryin","jones","hamza","rathour","mondak","kohli","dhawan","silva","dhoni","patil","poker","pronjal","piku","pondu","randi","rawdi","jalbbi","julmi","joker","khan","chaubey","ali","vishnoi","voni","ballad","billu","kalia","kuber","roberg","robert","ren","rewsi","modu",);
+$lname=$n[mt_rand(0,50)];
+
+function rando($length){
+$str="";
+$char='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$charArray= str_split($char);
+for($i=0;$i<$length;$i++){
+$randitem= array_rand($charArray);
+$str.="".$charArray[$randitem];
+}
+return $str;
+}
+$im=rando(4);
+$ik=rando(16);
+$sa=rando(8).'-'.rando(4).'-'.rando(4).'-'.rando(4).'-'.rando(12);
+
+function RandomNumber($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+$base_string = "ae835dcb573d048c";
+$length = strlen($base_string);
+for ($i = 0; $i < $length; $i++) {
+    $random_index = rand(0, $length - 1);
+    $device_id .= $base_string[$random_index];
+}
+
+$hash1=bin2hex(openssl_random_pseudo_bytes(16));
+$de=Rando(16);
+$app_version="2.0.58";
+$hash=md5("|njt|-$de-|njt|-$de-|njt|-$app_version-|njt|");
+
+$ip = mt_rand(0, 255) . "." . mt_rand(0, 255) . "." . mt_rand(0, 255) . "." . mt_rand(0, 255);
+
+$digit_10=RandomNumber(10);
+$digit_9=RandomNumber(9);
+$digit_8=RandomNumber(8);
+$digit_7=RandomNumber(7);
+$digit_6=RandomNumber(6);
+$digit_5=RandomNumber(5);
+$digit_4=RandomNumber(4);
+$digit_3=RandomNumber(3);
+$digit_2=RandomNumber(2);
+$digit_1=RandomNumber(1);
+$email = "$fname$lname$digit_4@gmail.com";
+$name = "$fname $lname";
+
+$mo = $_GET['mo'];
+$ref = $_GET['ref'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => 'https://tathya.uidai.gov.in/audioCaptchaService/api/captcha/v3/generation',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => '{"captchaLength":"6","captchaType":"2","audioCaptchaRequired":true}',
+  CURLOPT_HTTPHEADER => [
+    'User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+    'Accept: application/json, text/plain, */*',
+    'Accept-Encoding: gzip, deflate, br, zstd',
+    'Content-Type: application/json',
+    'sec-ch-ua: "Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-mobile: ?1',
+    'appid: MYAADHAAR',
+    'X-Request-ID: db53f65d-bb44-4652-a5ee-98f2438e59c2',
+    'sec-ch-ua-platform: "Android"',
+    'Origin: https://myaadhaar.uidai.gov.in',
+    'Sec-Fetch-Site: same-site',
+    'Sec-Fetch-Mode: cors',
+    'Sec-Fetch-Dest: empty',
+    'Referer: https://myaadhaar.uidai.gov.in/',
+    'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,bn;q=0.6',
+  ],
+]);
+
+$output=curl_exec($curl);
+$json=json_decode($output,true);
+$msg=$json['imageBase64'];
+$msg1=$json['transactionId'];
+
+echo "<form mehod='GET' action='eid_to_aadhaar1.php'>
+<input type='hidden' name='captchaTxnId' value='$msg1'>
+
+        <div class='form-group'>
+        <div style='display: flex; align-items: center;'>
+            <img id='captchaImage' src='data:image/jpeg;base64,$msg' alt='Captcha' class='babu' width='100' height='50'>
+            <div class='form-group'>
+                
+            <button type='button' class='captcha-refresh' onclick='refreshCaptcha()' style='background-color: transparent; border: none;'><img src='1024px-Refresh_icon.svg.png' alt='Refresh Captcha' width='30' height='30'></button>
+        </div></div>
+        
+        <div class='form-group'>
+            <input type='text' name='captcha' placeholder='Enter Captcha' class='input' required>
+            
+            <input type='text' name='eid' placeholder='Enter 28 digit Enrolment Number' class='input' required>
+            
+            <input type='text' name='mo' placeholder='Enter Any Mobile Number' class='input' required>
+
+        <button type='submit' name='submit' value='Submit' style='background-image: linear-gradient(to right, rgb(2, 13, 81), rgb(25, 176, 220)); color: white;'>Proceed</button>
+    </form>";
+?>
+
+<script>
+function convertToLowercase() {
+  var inputText = document.getElementById("textInput").value;
+  var lowercaseText = inputText.toLowerCase();
+  document.getElementById("textInput").value = lowercaseText;
+}
+</script>
+
+<script>
+        function refreshCaptcha() {
+
+            location.reload();
+        }
+    </script>
+    
+    </div></div><script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+    <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+    .card {
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      max-width: 90%;
+      margin: auto;
+      text-align: center;
+      font-family: arial;
+      padding: 20px;
+    }
+  </style>
+</head>
+<body>
+    </div></div></div></div><br> <div class="card">
+
+<script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//telegram.im/widget-button/index.php?id=@TechCampaing";document.getElementsByTagName("head")[0].appendChild(script);})();</script><a href="tg://resolve?domain=TechCampaing" target="_blank" class="telegramim_button telegramim_shadow telegramim_pulse" style="font-size:18px;max-width:100%;background:#27A5E7;box-shadow:1px 1px 5px #27A5E7;color:#ffffff;border-radius:50px;" title="Join Now"><i></i><img src='  ' style='width: 1px; margin-left: 1px;'><b> Join Our Telegram Channel<img src='  ' style='width: 1px; margin-left: 1px;'></b></a></center>
+</div>
+<br>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+</body>
+</html>
